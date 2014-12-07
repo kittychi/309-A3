02fd7f54b27a38412debc63845882726ce9eedfa
J-Triple <Jason_Harrison@live.ca>
2014-12-07 00:21:08 -0500

    Captcha test 1

--- A4/application/controllers/account.php
+++ A4/application/controllers/account.php
@@ -96,6 +96,21 @@ class Account extends CI_Controller {
 
 	    		$this->load->model('user_model');
 	    		$error = $this->user_model->insert($user);
+
+	    		// Captcha check
+	    		include_once $_SERVER['DOCUMENT_ROOT'] . '/securimage/securimage.php';
+				$securimage = new Securimage();
+
+				if ($securimage->check($_POST['captcha_code']) == false) {
+					// the code was incorrect
+					// you should handle the error so that the form processor doesn't continue
+
+					// or you can use the following code if there is no validation or you do not know how
+					echo "The security code entered was incorrect.<br /><br />";
+					echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+					exit;
+				}
+
 	    		
 	    		$this->load->view('account/loginForm');
 	    	}
